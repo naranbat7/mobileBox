b19d9145497bc3516a16d6d2a9e728fb3571bd53
naranbat <m.naranbat7@gmail.com>
2021-01-19 22:08:56 +0800

    env changed

--- config/database.js
+++ config/database.js
@@ -3,13 +3,11 @@ const { request } = require("express");
 const { createPool } = require("mysql");
 
 const pool = createPool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    connectionLimit: 10,
-    insecureAuth: true,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 module.exports = pool;
