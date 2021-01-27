c0bdce9e3a15c9422bb173573209b301588877d7
naranbat <m.naranbat7@gmail.com>
2021-01-27 15:44:10 +0800

    email fixed

--- api/user/user.service.js
+++ api/user/user.service.js
@@ -162,7 +162,7 @@ module.exports = {
 
     // create reusable transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
-      host: "smtp.ethereal.email",
+      host: "smtp.gmail.com",
       port: 587,
       secure: false, // true for 465, false for other ports
       auth: {
